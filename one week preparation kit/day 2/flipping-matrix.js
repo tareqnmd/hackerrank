50fd6cef6c3269e51f47a89af5d075d92e3ee3d9
Tareq <tareqnmd@gmail.com>
2024-03-16 00:16:05 +0600

    flip

--- one week preparation kit/day 2/flipping-matrix.js
+++ one week preparation kit/day 2/flipping-matrix.js
@@ -13,3 +13,12 @@ function flippingMatrix(matrix) {
 	}
 	return maxSum;
 }
+
+console.log(
+	flippingMatrix([
+		[112, 42, 83, 119],
+		[56, 125, 56, 49],
+		[15, 78, 101, 43],
+		[62, 98, 114, 108],
+	])
+);
